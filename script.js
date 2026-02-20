1ae7ddc90ef946585f9924c50625dd5bcc55dfb4
zul-eth <154829549+zul-eth@users.noreply.github.com>
2026-02-20 22:34:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function checkEnv() {
   const status = document.getElementById("status");
 
-  if (window.base) {
-    status.innerText = "✅ Running inside Base App";
-    console.log("Base object:", window.base);
+  console.log("window.base =", window.base);
+  console.log("window.ethereum =", window.ethereum);
+
+  if (window.base || window.ethereum) {
+    status.innerText = "✅ Running inside Base App (Provider Detected)";
   } else {
     status.innerText = "🌐 Running in normal browser";
   }
